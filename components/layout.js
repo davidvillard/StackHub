6106cf2b603e4b70352c75fc62012934bfc6d30a
David Villar Durán <davidvillard6@gmail.com>
2025-04-30 22:33:34 +0200

    cambios mobile responsive

--- components/layout.js
+++ components/layout.js
@@ -2,7 +2,7 @@ import Sidebar from "../components/sidebar";
 
 const Layout = ({ children }) => {
   return (
-    <div className="flex flex-row sm:flex-row h-screen">
+    <div className="flex flex-row sm:flex-row h-[100dvh]">
       <Sidebar />
       <div className="flex-1 flex flex-col overflow-auto bg-neutral-800 text-white p-4">
         {children}
