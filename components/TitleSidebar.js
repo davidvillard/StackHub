aa14a91f304ce99fb3277a37082463f882f03623
David Villar Durán <davidvillard6@gmail.com>
2025-05-04 22:59:07 +0200

    change image to 3D

--- components/TitleSidebar.js
+++ components/TitleSidebar.js
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function TitleSidebar({ collapsed }) {
   return (
-    <div className={`flex items-center ${collapsed ? 'justify-center' : 'justify-between'} gap-2 mb-8 mt-12 md:mb-8 md:mt-0 px-4`}>
+    <div className={`flex items-center ${collapsed ? 'justify-center px-2' : 'justify-between px-6'} gap-2 mb-8 mt-12 md:mb-8 md:mt-0`}>
       {!collapsed && <h1 className="clash-title text-xl font-bold">Free Stack</h1>}
       <Image 
         src="/StackHub_3D.png" 
