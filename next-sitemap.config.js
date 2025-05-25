e081d519a63196b2104ec532f60657c366ce9ba8
p4zd <davidvillard6@gmail.com>
2025-05-25 11:49:03 +0200

    fix some errors

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   siteUrl: 'https://stackhub.netlify.app',
-  generateRobotsTxt: true, // (optional)
-  // ...other options
+  generateRobotsTxt: true,
+  outDir: 'public', // Asegura que se escriba en la carpeta public
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: "*", allow: "/" },
+      { userAgent: "*", disallow: ["/_next/", "/api/", "/404", "/500", "/*.json$"] },
+    ],
+  },
 }
