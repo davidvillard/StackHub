a813f28a796f8be98c81e86bb77cba8a5502b38f
David Villar Durán <davidvillard6@gmail.com>
2025-05-26 23:15:43 +0200

    add meta search console

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,11 +1,20 @@
 module.exports = {
   siteUrl: 'https://stackhub.netlify.app',
   generateRobotsTxt: true,
-  outDir: 'public', // Asegura que se escriba en la carpeta public
+  outDir: 'public',
   robotsTxtOptions: {
     policies: [
-      { userAgent: "*", allow: "/" },
-      { userAgent: "*", disallow: ["/_next/", "/api/", "/404", "/500", "/*.json$"] },
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '/_next/',
+          '/api/',
+          '/404',
+          '/500',
+          '/*.json$',
+        ],
+      },
     ],
   },
-}
+};
