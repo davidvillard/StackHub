a1c689f3e115714a6c081d6e3ce1d29c7e3537cc
p4zd <davidvillard6@gmail.com>
2025-05-29 22:53:48 +0200

    fix: favicon error

--- pages/_document.js
+++ pages/_document.js
@@ -5,12 +5,12 @@ export default function Document() {
     <Html lang="es">
       <Head>
         {/* El meta viewport ya está configurado por Next.js */}
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <meta name="theme-color" content="#000000" />
+        <link rel="icon" type="image/png" href="%PUBLIC_URL%/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="%PUBLIC_URL%/favicon.svg" />
+        <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="%PUBLIC_URL%/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="MyWebSite" />
+        <link rel="manifest" href="%PUBLIC_URL%/site.webmanifest" />
       </Head>
       <body className="antialiased">
         <Main />
