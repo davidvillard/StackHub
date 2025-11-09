4fb2a4865f1939db520c0a4b8e5b6982a355ef9b
David Villar Durán <davidvillard6@gmail.com>
2025-11-09 20:49:17 +0100

    Arreglo imagen logo y estilos

--- components/TitleSidebar.js
+++ components/TitleSidebar.js
@@ -5,13 +5,12 @@ export default function TitleSidebar({ collapsed }) {
     <div className={`flex items-center ${collapsed ? 'justify-center px-2' : 'justify-between px-6'} gap-2 mb-8 mt-12 md:mb-8 md:mt-0`}>
       {!collapsed && <h1 className="clash-title text-xl font-bold">StackHub</h1>}
       <Image 
-        src="/StackHub_3D.png" 
+        src="/StackHub_3D.PNG" 
         alt="StackHub Logo"
         width={collapsed ? 40 : 48}
         height={collapsed ? 40 : 48}
         priority
         className={`animate-bounceSlow2 ${collapsed ? 'mx-auto' : ''}`}
-        unoptimized
       />
     </div>
   );
